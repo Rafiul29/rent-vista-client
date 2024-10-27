de2f203fd83ccbad53724ace04c125030c3302ff
Rafiul29 <rafiulislam4472@gmail.com>
2024-10-27 21:48:13 +0600

    make payment page aded

--- js/rent_request_and_favourite_rent.js
+++ js/rent_request_and_favourite_rent.js
@@ -1,87 +1,105 @@
-const userId = localStorage.getItem('userId');
-const token = localStorage.getItem('authToken');
-
-
+const userId = localStorage.getItem("userId");
+const token = localStorage.getItem("authToken");
 
 const getParams = () => {
   const param = new URLSearchParams(window.location.search).get("advertiseId");
-  return param
-}
-
-
-
+  return param;
+};
 
 const handleRequestRent = () => {
-
-  const param = getParams()
+  const param = getParams();
 
   if (!userId && !token) {
-    window.location.href = 'login.html'
-    return
+    window.location.href = "login.html";
+    return;
   }
+    window.location.href=`make_payment_information.html?advertiseId=${param}`;
+  console.log("yes")
 
-  // check is already rent request 
-  fetch(`${BASE_URL}/advertisement/rent_request/?requester_id=${userId}`, {
-    headers: {
-      'content-type': 'application/json',
-      'Authorization': `Token ${token}`,
-    },
-  })
-    .then((res) => res.json())
-    .then((data) => {
-      const isRequest = data?.filter((req => req.advertisement == getParams() && req.requester))
+  // check is already rent request
+  // fetch(`${BASE_URL}/advertisement/rent_request/?requester_id=${userId}`, {
+  //   headers: {
+  //     'content-type': 'application/json',
+  //     'Authorization': `Token ${token}`,
+  //   },
+  // })
+  //   .then((res) => res.json())
+  //   .then((data) => {
+  //     const isRequest = data?.filter((req => req.advertisement == getParams() && req.requester))
 
-      if (isRequest.length == 0) {
-        fetch(`${BASE_URL}/advertisement/rent_request/`, {
-          method: "POST",
-          headers: {
-            "content-type": "application/json",
-            'Authorization': `Token ${token}`
-          },
-          body: JSON.stringify({
-            advertisement: param,
-            requester: userId
-          })
-        }).then(res => res.json())
-          .then(data => {
-            console.log(data)
-            if (data?.id) {
-              window.location.href = 'my_requested_rent.html'
-            }
-            if(data?.error){
-              alert(`${data.error}!.Please deposit account balance`)
-               window.location.href = 'deposit_balance.html'
-            }
-          })
-      } else {
-        alert("Already Send Rent Request")
-      }
-    });
+  //     if (isRequest.length == 0) {
+  //       fetch(`${BASE_URL}/advertisement/rent_request/`, {
+  //         method: "POST",
+  //         headers: {
+  //           "content-type": "application/json",
+  //           'Authorization': `Token ${token}`
+  //         },
+  //         body: JSON.stringify({
+  //           advertisement: param,
+  //           requester: userId
+  //         })
+  //       }).then(res => res.json())
+  //         .then(data => {
+  //           if (data?.id) {
+  //             window.location.href = 'my_requested_rent.html'
+  //           }
+  //           if(data?.error){
+  //             alert(`${data.error}!.Please deposit account balance`)
+  //              window.location.href = 'deposit_balance.html'
+  //           }
+  //         })
+  //     } else {
+  //       alert("Already Send Rent Request")
+  //     }
+  //   });
 
-}
 
+  // fetch(`${BASE_URL}/advertisement/rent_request/`, {
+  //   method: "POST",
+  //   headers: {
+  //     "content-type": "application/json",
+  //     Authorization: `Token ${token}`,
+  //   },
+  //   body: JSON.stringify({
+  //     advertisement: param,
+  //     requester: userId,
+  //   }),
+  // })
+  //   .then((res) => res.json())
+  //   .then((data) => {
+  //     if (data?.id) {
+  //       window.location.href = "my_requested_rent.html";
+  //     }
+  //     if (data?.error) {
+  //       alert(`${data.error}!.Please deposit account balance`);
+  //       window.location.href = "deposit_balance.html";
+  //     }
+  //   });
+
+};
 
 const handleFavouriteRent = () => {
-  const param = getParams()
+  const param = getParams();
 
   if (!userId && !token) {
-    window.location.href = 'login.html'
-    return
+    window.location.href = "login.html";
+    return;
   }
   fetch(`${BASE_URL}/advertisement/favourite/`, {
     method: "POST",
     headers: {
       "content-type": "application/json",
-      'Authorization': `Token ${token}`
+      Authorization: `Token ${token}`,
     },
     body: JSON.stringify({
       advertisement: param,
-      user: userId
-    })
-  }).then(res => res.json())
-    .then(data => {
+      user: userId,
+    }),
+  })
+    .then((res) => res.json())
+    .then((data) => {
       if (data?.id) {
-        window.location.href = 'favourite_rent.html'
+        window.location.href = "favourite_rent.html";
       }
-    })
-}
+    });
+};
